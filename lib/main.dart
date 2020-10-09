6e4a2fcbe23d00b24ba85ffa6d355a9ff4af5af0
Cesar Lopez <Lopez.Cesar25@hotmail.com>
2020-10-09 13:47:53 -0500

    Se creo una carpeta de rutas con una función para llamar dichas

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
-
-import 'package:componentes/src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,12 +10,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Componentes App',
-      // home: HomePage(),
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/' : (BuildContext context) => HomePage(),
-        'alert' : (BuildContext context) => AlertPage(),
-        'avatar' : (BuildContext context) => AvatarPage(),
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (RouteSettings settings) {
+        return MaterialPageRoute(
+            builder: (BuildContext context) => AlertPage());
       },
     );
   }
