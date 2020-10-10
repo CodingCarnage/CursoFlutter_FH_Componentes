75e25365171b1bf80fdfb45f475a8a62158eed9b
Cesar Lopez <Lopez.Cesar25@hotmail.com>
2020-10-10 17:32:41 -0500

    Crear un Card Widget con una Image y un FadeIn Image

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -13,6 +13,8 @@ class CardPage extends StatelessWidget {
         padding: EdgeInsets.all(10.0),
         children: <Widget>[
           _cardType1(),
+          SizedBox(height: 30.0),
+          _cardType2(),
         ],
       ),
     );
@@ -28,8 +30,7 @@ class CardPage extends StatelessWidget {
               color: Colors.blue,
             ),
             title: Text('Soy el titulo'),
-            subtitle: Text(
-                'Estas fgafsddgwerqwerqwe werqouiajgasd qweorijsdifjawenhr'),
+            subtitle: Text('Estas fgafsddgwerqwerqwe werqouiajgasd qweorijsdifjawenhr'),
           ),
           Row(
             children: <Widget>[
@@ -44,6 +45,26 @@ class CardPage extends StatelessWidget {
             ],
             mainAxisAlignment: MainAxisAlignment.end,
           )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardType2() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            image: NetworkImage('https://www.wallpaperup.com/uploads/wallpapers/2016/06/24/991790/d064e15142525dca13c46c7d22eb779c-1000.jpg'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('No tengo que poner'),
+          ),
         ],
       ),
     );
